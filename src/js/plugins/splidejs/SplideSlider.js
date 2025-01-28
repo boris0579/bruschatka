4bebd244ad6ed486f5a0e07de370ed5f122579cb
Boris <bobabonanadz@mail.ru>
2025-01-28 10:05:49 +0300

    Fixed sliders

--- src/js/plugins/splidejs/SplideSlider.js
+++ src/js/plugins/splidejs/SplideSlider.js
@@ -19,21 +19,30 @@ export default class SplideSlider {
      * Метод для инициализации слайдера
      */
     init () {
-        const element = document.querySelector(
+        const elements = document.querySelectorAll(
             `[data-splide="${this.attribute}"] .splide`
         )
 
-        if (!element) {
-            console.warn(`Элемент с атрибутом "${this.attribute}" не найден.`)
+        if (elements.length === 0) {
+            console.warn(`Элементы с атрибутом "${this.attribute}" не найдены.`)
             return
         }
 
-        try {
-            this.splide = new Splide(element, this.options)
-            this.splide.mount()
-        } catch (error) {
-            console.error('Ошибка при инициализации слайдера Splide:', error)
-        }
+        this.sliders = [] // Очищаем массив перед инициализацией
+
+        elements.forEach((element, index) => {
+            try {
+                const splide = new Splide(element, this.options)
+                splide.mount()
+                this.sliders.push(splide) // Сохраняем экземпляр в массив
+                console.log(`Слайдер #${index + 1} успешно инициализирован.`)
+            } catch (error) {
+                console.error(
+                    `Ошибка при инициализации слайдера #${index + 1}:`,
+                    error
+                )
+            }
+        })
     }
 
     /**
