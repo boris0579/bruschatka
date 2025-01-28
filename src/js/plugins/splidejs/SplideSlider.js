dbcaa76c478474df94035b83bfcd67f0254292df
Boris <bobabonanadz@mail.ru>
2025-01-28 17:28:27 +0300

    Fixed slider

--- src/js/plugins/splidejs/SplideSlider.js
+++ src/js/plugins/splidejs/SplideSlider.js
@@ -1,8 +1,5 @@
-import Splide from '@splidejs/splide' // Импортируем библиотеку Splide
+import Splide from '@splidejs/splide'
 
-/**
- * Класс для управления слайдерами Splide
- */
 export default class SplideSlider {
     /**
      * Создаёт экземпляр слайдера
@@ -19,30 +16,52 @@ export default class SplideSlider {
      * Метод для инициализации слайдера
      */
     init () {
+        // Находим все элементы с данным атрибутом
         const elements = document.querySelectorAll(
             `[data-splide="${this.attribute}"] .splide`
         )
 
+        // Если слайдеров не найдено, выводим предупреждение и завершаем выполнение
         if (elements.length === 0) {
-            console.warn(`Элементы с атрибутом "${this.attribute}" не найдены.`)
+            console.warn(`Элемент с атрибутом "${this.attribute}" не найден.`)
             return
         }
 
-        this.sliders = [] // Очищаем массив перед инициализацией
+        // Инициализация массива для хранения экземпляров слайдеров
+        this.sliders = []
 
-        elements.forEach((element, index) => {
+        // Если найден только один слайдер
+        if (elements.length === 1) {
             try {
-                const splide = new Splide(element, this.options)
+                // Инициализируем слайдер
+                const splide = new Splide(elements[0], this.options)
                 splide.mount()
-                this.sliders.push(splide) // Сохраняем экземпляр в массив
-                console.log(`Слайдер #${index + 1} успешно инициализирован.`)
+                this.splide = splide // Сохраняем экземпляр в this.splide для дальнейшего использования
             } catch (error) {
                 console.error(
-                    `Ошибка при инициализации слайдера #${index + 1}:`,
+                    'Ошибка при инициализации слайдера Splide:',
                     error
                 )
             }
-        })
+        } else {
+            // Если найдено несколько слайдеров
+            elements.forEach((element, index) => {
+                try {
+                    // Инициализируем каждый слайдер
+                    const splide = new Splide(element, this.options)
+                    splide.mount()
+                    this.sliders.push(splide) // Добавляем экземпляр слайдера в массив
+                    console.log(
+                        `Слайдер #${index + 1} успешно инициализирован.`
+                    )
+                } catch (error) {
+                    console.error(
+                        `Ошибка при инициализации слайдера #${index + 1}:`,
+                        error
+                    )
+                }
+            })
+        }
     }
 
     /**
