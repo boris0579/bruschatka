22e25d3466c3a109825edcb94d7bea551f5293a9
Boris <bobabonanadz@mail.ru>
2025-01-29 18:36:32 +0300

    Fixed slider categories

--- src/js/plugins/splidejs/initSlider.js
+++ src/js/plugins/splidejs/initSlider.js
@@ -42,6 +42,8 @@ export default function initSlider () {
         pagination: false,
         drag: 'free',
         live: false,
+        // waitForTransition: true,
+        wheel: true,
         breakpoints: {
             768: {
                 type: 'loop'
