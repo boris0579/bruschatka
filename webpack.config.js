9560bcaaaec797189c915ddc1f2407555687115b
Boris <bobabonanadz@mail.ru>
2025-08-13 11:02:31 +0300

    fixed urls v1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const fs = require('fs') // Для работы с файловой системой
+const fs = require('fs')
 
 // Путь к папке со страницами
 const pagesDir = path.resolve(__dirname, 'src/pages')
@@ -15,32 +15,42 @@ const pageFiles = fs
 // Создаем массив HtmlWebpackPlugin для каждой страницы
 const htmlPagesPlugins = pageFiles.map(file => {
     return new HtmlWebpackPlugin({
-        template: path.join(pagesDir, file), // путь к исходному HTML
-        filename: `pages/${file}` // путь в папке dist
+        template: path.join(pagesDir, file),
+        filename: `pages/${file}`
     })
 })
 
 module.exports = {
-    entry: './src/js/main.js', // основной файл JS
+    entry: './src/js/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.[contenthash].js',
-        assetModuleFilename: 'assets/[name][ext]'
+        assetModuleFilename: 'assets/[name][ext]',
+        publicPath: '/bruschatka/' // Префикс для всех путей
     },
     mode: 'development', // или 'production'
     devServer: {
         static: [
-            path.join(__dirname, 'dist'), // Сборка
-            path.join(__dirname, 'src/pages') // Для работы с исходниками страниц без сборки
+            {
+                directory: path.join(__dirname, 'dist'),
+                publicPath: '/bruschatka/' // маппинг dist в /bruschatka/
+            },
+            {
+                directory: path.join(__dirname, 'src/pages'),
+                publicPath: '/bruschatka/pages/'
+            }
         ],
         port: 3000,
-        open: true
+        open: ['/bruschatka/'], // откроет сразу /bruschatka/
+        historyApiFallback: {
+            index: '/bruschatka/index.html'
+        }
     },
     module: {
         rules: [
             {
                 test: /\.html$/i,
-                loader: 'html-loader' // для импорта HTML
+                loader: 'html-loader'
             },
             {
                 test: /\.scss$/i,
@@ -48,17 +58,17 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resource' // для загрузки изображений
+                type: 'asset/resource'
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource' // для загрузки шрифтов
+                type: 'asset/resource'
             },
             {
                 test: /\.js$/i,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader', // трансформация JS для совместимости
+                    loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env']
                     }
@@ -69,10 +79,10 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: './src/index.html', // шаблон HTML
+            template: './src/index.html',
             filename: 'index.html'
         }),
-        ...htmlPagesPlugins, // Подключаем автоматически созданные плагины для страниц
+        ...htmlPagesPlugins,
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css'
         })
